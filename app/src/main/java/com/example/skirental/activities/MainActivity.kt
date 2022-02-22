7504c66eb432a29442d6eb513602230c0864f8a4
vlad <vlad.ricean@brivo.com>
2022-02-22 09:31:08 +0200

    -added packages for activities/ fragments

--- app/src/main/java/com/example/skirental/activities/MainActivity.kt
+++ app/src/main/java/com/example/skirental/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.skirental
+package com.example.skirental.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.skirental.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
