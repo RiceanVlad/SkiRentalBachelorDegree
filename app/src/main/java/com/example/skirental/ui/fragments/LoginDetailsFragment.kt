394ba85d176a930d7514853160bbf500910c90c5
vlad <vlad.ricean@brivo.com>
2022-02-28 09:17:18 +0200

    -add login details fragment

---
+++ app/src/main/java/com/example/skirental/ui/fragments/LoginDetailsFragment.kt
@@ -0,0 +1,33 @@
+package com.example.skirental.ui.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.skirental.viewmodels.LoginDetailsViewModel
+import com.example.skirental.R
+
+class LoginDetailsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = LoginDetailsFragment()
+    }
+
+    private lateinit var viewModel: LoginDetailsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.login_details_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(LoginDetailsViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/skirental/viewmodels/LoginDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.skirental.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class LoginDetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
