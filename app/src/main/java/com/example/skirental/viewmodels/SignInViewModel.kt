7f0060c78892e646fc434820b908efe1f97c2d25
vlad <vlad.ricean@brivo.com>
2022-03-11 09:43:33 +0200

    -add sign in Logic

--- app/src/main/java/com/example/skirental/viewmodels/SignInViewModel.kt
+++ app/src/main/java/com/example/skirental/viewmodels/SignInViewModel.kt
@@ -2,7 +2,15 @@ package com.example.skirental.viewmodels
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.skirental.utils.SingleLiveEvent
+import timber.log.Timber
 
 class SignInViewModel: ViewModel() {
+    private val _onSignInClicked = SingleLiveEvent<Void>()
+    val onSignInClicked : LiveData<Void> = _onSignInClicked
 
+    fun onSignInClicked() {
+        Timber.i("LUL")
+        _onSignInClicked.call()
+    }
 }
