5776d44320cb611e2c6228ff2aa7ef4054c21c74
vlad <vlad.ricean@brivo.com>
2022-03-15 19:39:37 +0200

    -added seekbar for ski experience

--- app/src/main/java/com/example/skirental/ui/fragments/LoginDetailsFragment.kt
+++ app/src/main/java/com/example/skirental/ui/fragments/LoginDetailsFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.SeekBar
+import android.widget.Toast
 import com.example.skirental.viewmodels.LoginDetailsViewModel
 import com.example.skirental.R
 import com.example.skirental.databinding.LoginDetailsFragmentBinding
@@ -26,8 +28,34 @@ class LoginDetailsFragment : Fragment() {
         binding.viewModel = viewModel
 
         setupSpinnerAdapters()
+        seekbarDisplayToast()
 
         return binding.root
+    }
+
+    private fun seekbarDisplayToast() {
+            binding.seekbarExperience.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+//                dificultate = progress
+                if (progress < 1)
+                    Toast.makeText(requireContext(), "Beginner", Toast.LENGTH_SHORT).show()
+                if (progress == 2 || progress == 3)
+                    Toast.makeText(requireContext(), "Intermediate", Toast.LENGTH_SHORT).show()
+                if (progress > 3 && progress < 5)
+                    Toast.makeText(requireContext(), "Advanced", Toast.LENGTH_SHORT).show()
+                if (progress == 5)
+                    Toast.makeText(requireContext(), "Pro", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                //Toast.makeText(applicationContext,"start tracking",Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                //Toast.makeText(applicationContext,"stop tracking",Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 
     private fun setupSpinnerAdapters() {
