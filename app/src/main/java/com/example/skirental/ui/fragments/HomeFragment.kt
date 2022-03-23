64edc0d60247e65e1856d762b81f51addec570f3
vlad <vlad.ricean@brivo.com>
2022-03-23 12:07:52 +0200

    -add navigation up & options menu

--- app/src/main/java/com/example/skirental/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/skirental/ui/fragments/HomeFragment.kt
@@ -2,10 +2,11 @@ package com.example.skirental.ui.fragments
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.skirental.R
 import com.example.skirental.databinding.HomeFragmentBinding
 import com.example.skirental.viewmodels.HomeViewModel
@@ -27,7 +28,23 @@ class HomeFragment : Fragment() {
         binding = HomeFragmentBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
+        binding.btnWeather.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_weatherFragment)
+        }
+        setHasOptionsMenu(true)
+
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI
+            .onNavDestinationSelected(item,requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
