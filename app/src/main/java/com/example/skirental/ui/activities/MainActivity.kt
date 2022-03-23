86f68a7d720245ee16e9c7836e551070e4ba74e8
vlad <vlad.ricean@brivo.com>
2022-03-23 12:33:03 +0200

    -nav drawer working (without nav_header)

--- app/src/main/java/com/example/skirental/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/skirental/ui/activities/MainActivity.kt
@@ -2,20 +2,27 @@ package com.example.skirental.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.skirental.R
+import com.example.skirental.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
         NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
