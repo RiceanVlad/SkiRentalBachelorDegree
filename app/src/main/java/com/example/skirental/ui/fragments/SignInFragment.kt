9e8f56d31821d9df9f883a4d6daf3c5a0b56a947
vlad <vlad.ricean@brivo.com>
2022-03-25 19:03:30 +0200

    -moved code from SignInActivity to SignInFragment

--- app/src/main/java/com/example/skirental/ui/fragments/SignInFragment.kt
+++ app/src/main/java/com/example/skirental/ui/fragments/SignInFragment.kt
@@ -1,29 +1,124 @@
 package com.example.skirental.ui.fragments
 
+import android.app.Activity
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.skirental.R
 import com.example.skirental.databinding.SignInFragmentBinding
 import com.example.skirental.viewmodels.SignInViewModel
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import timber.log.Timber
 
 class SignInFragment : Fragment() {
 
+    private val db = Firebase.firestore
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var viewModel: SignInViewModel
     private lateinit var binding: SignInFragmentBinding
+
+    private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            signInWithGoogle(data)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         viewModel = ViewModelProvider(this)[SignInViewModel::class.java]
         binding = SignInFragmentBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
 
+        setupObservers()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id)) // it is generated
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
+        mAuth = FirebaseAuth.getInstance()
+
         return binding.root
+    }
+
+    private fun setupObservers() {
+//        viewModel.onSignInClicked.observe(this,
+//            Observer{
+//                createIntentSigninGoogle()
+//            })
+    }
+
+    private fun createIntentSigninGoogle() {
+        val signInIntent = googleSignInClient.signInIntent
+        resultLauncher.launch(signInIntent)
+    }
+
+    private fun signInWithGoogle(data: Intent?) {
+        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+        val exception = task.exception
+        if(task.isSuccessful) {
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Timber.d("firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                Timber.w("Google sign in failed", e)
+            }
+        } else {
+            Timber.w(exception.toString())
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        mAuth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    Timber.d( "signInWithCredential:success")
+                    addUserToFirebase()
+//                    val intent = Intent(this, MainActivity::class.java)
+//                    startActivity(intent)
+//                    finish()
+                } else {
+                    Timber.w( "signInWithCredential:failure", task.exception)
+                }
+            }
+    }
+
+    private fun addUserToFirebase() {
+        val currentUser = mAuth.currentUser
+        val user = hashMapOf(
+            "name" to currentUser!!.displayName,
+            "score" to 0
+        )
+        db.collection("users").document(currentUser.uid)
+            .set(user)
+            .addOnSuccessListener { documentReference ->
+                Timber.i("User ${currentUser.displayName} has been added to firestore")
+            }
+            .addOnFailureListener { e ->
+                Timber.w( "Error adding document", e)
+            }
     }
 
 
