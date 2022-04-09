4472168193df418b98becd6d8a2a3d022a85cd10
vlad <vlad.ricean@brivo.com>
2022-04-09 16:06:02 +0300

    -navigate to equipment fragment

--- app/src/main/java/com/example/skirental/viewmodels/SearchViewModel.kt
+++ app/src/main/java/com/example/skirental/viewmodels/SearchViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.skirental.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.skirental.utils.SingleLiveEvent
 
 class SearchViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _onNavigateToEquipmentFragment = SingleLiveEvent<Void>()
+    val onNavigateToEquipmentFragment : LiveData<Void> = _onNavigateToEquipmentFragment
+
+    fun onNavigateToEquipmentFragment() {
+        _onNavigateToEquipmentFragment.call()
+    }
 }
