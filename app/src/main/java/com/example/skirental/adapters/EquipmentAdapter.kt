7d3eb771f13f633222cb347f9ab7b820a1f8f4e6
vlad <vlad.ricean@brivo.com>
2022-04-09 15:48:53 +0300

    -implemented adapter

--- app/src/main/java/com/example/skirental/adapters/EquipmentAdapter.kt
+++ app/src/main/java/com/example/skirental/adapters/EquipmentAdapter.kt
@@ -1,5 +1,49 @@
 package com.example.skirental.adapters
 
-class EquipmentAdapter {
-    
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.ListAdapter
+import com.example.skirental.databinding.ListItemEquipmentBinding
+import com.example.skirental.models.Equipment
+
+class EquipmentAdapter :
+    ListAdapter<Equipment, EquipmentAdapter.ViewHolder>(EquipmentDiffCallback()) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
+    }
+
+    class ViewHolder private constructor(val binding: ListItemEquipmentBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: Equipment) {
+            binding.equipment = item
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ListItemEquipmentBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+    }
+}
+
+class EquipmentDiffCallback : DiffUtil.ItemCallback<Equipment>() {
+
+    override fun areItemsTheSame(oldItem: Equipment, newItem: Equipment): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Equipment, newItem: Equipment): Boolean {
+        return oldItem == newItem
+    }
 }
---
+++ app/src/main/java/com/example/skirental/models/Equipment.kt
@@ -0,0 +1,7 @@
+package com.example.skirental.models
+
+data class Equipment (
+    val id: String = "",
+    val length: Int = 0,
+    val shoeSize: Int = 0,
+)
