edd9c0bf4805f371bd684a2f7c5348088b200d85
vlad <vlad.ricean@brivo.com>
2022-04-09 15:53:13 +0300

    -added Equipment Fragment

---
+++ app/src/main/java/com/example/skirental/ui/fragments/EquipmentFragment.kt
@@ -0,0 +1,28 @@
+package com.example.skirental.ui.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.skirental.databinding.EquipmentFragmentBinding
+import com.example.skirental.viewmodels.EquipmentViewModel
+
+class EquipmentFragment : Fragment() {
+
+    private lateinit var viewModel: EquipmentViewModel
+    private lateinit var binding: EquipmentFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        viewModel = ViewModelProvider(this)[EquipmentViewModel::class.java]
+        binding = EquipmentFragmentBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+        return  binding.root
+    }
+}
---
+++ app/src/main/java/com/example/skirental/viewmodels/EquipmentViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.skirental.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class EquipmentViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
