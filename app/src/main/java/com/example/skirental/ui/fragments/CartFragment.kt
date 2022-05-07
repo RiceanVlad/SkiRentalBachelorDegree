824a5fb6f617b65bafe99e90d672846d05123feb
vlad <vlad.ricean@brivo.com>
2022-05-07 15:20:06 +0300

    -use collectLatest on stateFlow without extension function

--- app/src/main/java/com/example/skirental/ui/fragments/CartFragment.kt
+++ app/src/main/java/com/example/skirental/ui/fragments/CartFragment.kt
@@ -7,9 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.skirental.databinding.CartFragmentBinding
 import com.example.skirental.utils.collectLatestLifecycleFlow
 import com.example.skirental.viewmodels.CartViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class CartFragment : Fragment() {
 
@@ -28,6 +33,14 @@ class CartFragment : Fragment() {
 //        collectLatestLifecycleFlow(viewModel.stateFlow) { number ->
 //            binding.tvState.text = number.toString()
 //        }
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.stateFlow.collectLatest { number ->
+                    binding.tvState.text = number.toString()
+                }
+            }
+        }
 
         collectLatestLifecycleFlow(viewModel.sharedFlow) {
             Toast.makeText(requireContext(), "LUL", Toast.LENGTH_SHORT).show()
