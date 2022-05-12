77617ba5a97efd59674f131d52bd20de43321ba6
vlad <vlad.ricean@brivo.com>
2022-05-12 16:37:27 +0300

    -load image into recyclerview from storage

--- app/src/main/java/com/example/skirental/utils/BindingAdapters.kt
+++ app/src/main/java/com/example/skirental/utils/BindingAdapters.kt
@@ -1,11 +1,14 @@
 package com.example.skirental.utils
 
+import android.content.Context
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
-import com.example.skirental.R
+import com.bumptech.glide.Glide
 import com.example.skirental.models.Equipment
+import com.example.skirental.ui.activities.MainActivity
 import com.google.android.gms.common.SignInButton
+import com.google.firebase.storage.FirebaseStorage
 
 @BindingAdapter("android:onClickGoogle")
 fun bindSignInClick(button: SignInButton, method: () -> Unit) {
@@ -25,10 +28,12 @@ fun TextView.setUsage(item: Equipment?) {
     }
 }
 
-@BindingAdapter("itemImage")
-fun ImageView.setItemImage(item: Equipment?) {
-    item?.let {
-        setImageResource(R.drawable.ic_cart)
-    }
+@BindingAdapter("loadImage")
+fun loadImage(imageView: ImageView?, imageUrl: String?) {
+    // Load image with your image loading library ,like with Glide or Picasso or any of your favourite
+    val storage = FirebaseStorage.getInstance()
+    val gsReference = storage.getReferenceFromUrl("gs://skirentallicenta-ef1a0.appspot.com/Skis/$imageUrl.jpeg")
+
+    imageView?.let { Glide.with(it.context).load(gsReference).into(imageView) }
 }
 
