d13618c0f6d9e633f50f3016f0c238bd4813a9c9
vlad <vlad.ricean@brivo.com>
2022-05-16 11:03:08 +0300

    -add 1 item to Cart via prefs working

--- app/src/main/java/com/example/skirental/ui/fragments/CartFragment.kt
+++ app/src/main/java/com/example/skirental/ui/fragments/CartFragment.kt
@@ -10,12 +10,21 @@ import android.widget.Toast
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import com.example.skirental.adapters.EquipmentAdapter
+import com.example.skirental.adapters.EquipmentListener
 import com.example.skirental.databinding.CartFragmentBinding
+import com.example.skirental.models.Equipment
+import com.example.skirental.models.User
+import com.example.skirental.utils.Prefs
 import com.example.skirental.utils.collectLatestLifecycleFlow
 import com.example.skirental.viewmodels.CartViewModel
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.FirebaseStorage
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
@@ -23,6 +32,10 @@ class CartFragment : Fragment() {
 
     private lateinit var viewModel: CartViewModel
     private lateinit var binding: CartFragmentBinding
+    private lateinit var adapter: EquipmentAdapter
+    private lateinit var prefs: Prefs
+    private lateinit var moshi: Moshi
+    private lateinit var jsonAdapter: JsonAdapter<Equipment>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,7 +45,25 @@ class CartFragment : Fragment() {
         binding = CartFragmentBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
+        prefs = Prefs(requireContext())
+        initializeMoshi()
+
+        adapter = EquipmentAdapter(EquipmentListener { equipment ->
+        })
+        binding.rvCartEquipmentList.adapter = adapter
+
+        val equipment = jsonAdapter.fromJson(prefs.cartItems ?: "")
+        val equipmentList: List<Equipment?> = listOf(equipment)
+        adapter.submitList(equipmentList)
 
         return  binding.root
     }
+
+    private fun initializeMoshi() {
+        moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+        jsonAdapter = moshi.adapter(Equipment::class.java)
+    }
+
 }
--- app/src/main/java/com/example/skirental/ui/fragments/DetailsEquipmentFragment.kt
+++ app/src/main/java/com/example/skirental/ui/fragments/DetailsEquipmentFragment.kt
@@ -9,13 +9,22 @@ import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import com.example.skirental.databinding.DetailsEquipmentFragmentBinding
+import com.example.skirental.models.Equipment
+import com.example.skirental.models.User
+import com.example.skirental.utils.Prefs
 import com.example.skirental.viewmodels.DetailsEquipmentViewModel
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 
 class DetailsEquipmentFragment : Fragment() {
 
     private lateinit var viewModel: DetailsEquipmentViewModel
     private lateinit var binding: DetailsEquipmentFragmentBinding
     private val args: DetailsEquipmentFragmentArgs by navArgs()
+    private lateinit var prefs: Prefs
+    private lateinit var moshi: Moshi
+    private lateinit var jsonAdapter: JsonAdapter<Equipment>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,9 +34,19 @@ class DetailsEquipmentFragment : Fragment() {
         binding = DetailsEquipmentFragmentBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
+        prefs = Prefs(requireContext())
+        initializeMoshi()
 
-        Toast.makeText(requireContext(), "${args.equipment.id} + ${args.equipment.type}", Toast.LENGTH_SHORT).show()
+//        Toast.makeText(requireContext(), "${args.equipment.id} + ${args.equipment.type}", Toast.LENGTH_SHORT).show()
+        prefs.cartItems = jsonAdapter.toJson(args.equipment)
 
         return binding.root
+    }
+
+    private fun initializeMoshi() {
+        moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+        jsonAdapter = moshi.adapter(Equipment::class.java)
     }
 }
