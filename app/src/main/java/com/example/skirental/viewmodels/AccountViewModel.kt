3118a0d6cc78fc21d5ef0c7d978028ccaa03c293
vlad <vlad.ricean@brivo.com>
2022-06-26 12:27:29 +0300

    -signOut functionality

--- app/src/main/java/com/example/skirental/viewmodels/AccountViewModel.kt
+++ app/src/main/java/com/example/skirental/viewmodels/AccountViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.skirental.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.skirental.utils.SingleLiveEvent
 
 class AccountViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val _onSignOutClicked = SingleLiveEvent<Void>()
+    val onSignOutClicked : LiveData<Void> = _onSignOutClicked
+
+    fun onSignOutClicked() {
+        _onSignOutClicked.call()
+    }
 }
