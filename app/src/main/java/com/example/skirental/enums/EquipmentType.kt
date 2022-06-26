0e61cd45a2d457ce5d444b92e7bf1a392a7c2b02
vlad <vlad.ricean@brivo.com>
2022-06-26 19:16:54 +0300

    add spinner to add equipment

--- app/src/main/java/com/example/skirental/enums/EquipmentType.kt
+++ app/src/main/java/com/example/skirental/enums/EquipmentType.kt
@@ -2,5 +2,10 @@ package com.example.skirental.enums
 
 enum class EquipmentType(val string: String) {
     SKI("SKI"),
-    SKI_BOOTS("SKI_BOOTS")
+    SKI_BOOTS("SKI_BOOTS"),
+//    POLES("POLES"),
+//    SNOWBOARD("SNOWBOARD"),
+//    SNOWBOARD_BOOTS("SNOWBOARD_BOOTS"),
+//    HELMET("HELMET"),
+//    GOGGLES("GOGGLES"),
 }
--- app/src/main/java/com/example/skirental/ui/fragments/AdminAddEquipmentFragment.kt
+++ app/src/main/java/com/example/skirental/ui/fragments/AdminAddEquipmentFragment.kt
@@ -6,9 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import com.example.skirental.R
 import com.example.skirental.databinding.FragmentAdminAddEquipmentBinding
 import com.example.skirental.databinding.StartFragmentBinding
+import com.example.skirental.enums.EquipmentType
 import com.example.skirental.viewmodels.AdminAddEquipmentViewModel
 import com.example.skirental.viewmodels.StartViewModel
 
@@ -25,8 +28,33 @@ class AdminAddEquipmentFragment : Fragment() {
         binding = FragmentAdminAddEquipmentBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
+        setupSpinnerAdapters()
 
         return binding.root
     }
 
+    private fun setupSpinnerAdapters() {
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.equipment_type_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            val equipmentTypeArray = resources.getStringArray(R.array.equipment_type_array)
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            binding.spinnerAddEquipmentType.adapter = adapter
+            binding.spinnerAddEquipmentType.onItemSelectedListener = object :
+                AdapterView.OnItemClickListener, AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(p0: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+                    val equipmentType = equipmentTypeArray[pos]
+                    binding.equipmentType = EquipmentType.valueOf(equipmentType)
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                }
+
+                override fun onItemClick(p0: AdapterView<*>?, view: View?, pos: Int, id: Long) {
+                }
+            }
+        }
+    }
 }
